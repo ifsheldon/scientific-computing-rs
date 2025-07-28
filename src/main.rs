477a4e3d671352625fa17ef399884dbfd83f2983
ifsheldon <feng.liang@kaust.edu.sa>
2025-07-28 16:52:27 +0800

    add generic benchmark function

--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,160 @@ use burn::prelude::{Backend, Tensor as BurnTensor};
 use burn::tensor::Distribution;
 use nalgebra::DMatrix;
 use rayon::prelude::*;
+use std::hint::black_box;
 use std::time::Instant;
-use tch::{set_num_interop_threads, set_num_threads};
 use tch::{Device, Kind, Tensor};
+use tch::{set_num_interop_threads, set_num_threads};
 
 fn main() {
     println!("Hello, here is a template for scientific computing in Rust with tch-rs");
+    let core_num = num_cpus::get();
+    println!("core_num: {core_num}");
+    rayon::ThreadPoolBuilder::new()
+        .num_threads(core_num)
+        .build_global()
+        .unwrap();
     let tensor_size = 200;
     let num_tensors = 2000;
     let repeat = 20;
-    // benchmark_parallelism_tch(tensor_size, num_tensors, repeat);
-    // benchmark_parallelism_nalgebra(tensor_size, num_tensors, repeat);
-    // benchmark_parallelism_burn(tensor_size, num_tensors, repeat);
-    benchmark_parallelism_candle(tensor_size, num_tensors, repeat);
-    // let a = dmatrix![Complex::new(1.0, 1.0), Complex::new(2.0, 1.0), Complex::new(3.0, 1.0);
-    // Complex::new(4.0, 1.0), Complex::new(5.0, 1.0), Complex::new(6.0, 1.0);
-    // Complex::new(7.0, 1.0), Complex::new(8.0, 1.0), Complex::new(9.0, 1.0)];
-    // let qr = a.clone().qr();
-    // println!("{:?}", qr.q());
-    // println!("{:?}", qr.r());
-    // let svd = a.svd(true, true);
-    // println!("{:?}", svd.u);
-    // println!("{:?}", svd.v_t);
+
+    // benchmarking burn
+    let burn_tensor_reduce_fn = |t: BurnTensor<NdArray, 2>| t.sum().into_scalar();
+    benchmark_with_into_iter(
+        tensor_size,
+        num_tensors,
+        repeat,
+        core_num,
+        get_randn_tensors_burn,
+        burn_tensor_reduce_fn,
+    );
+
+    // benchmarking candle
+    let candle_tensor_reduce_fn =
+        |t: &candle_core::Tensor| t.sum([0, 1]).unwrap().to_scalar::<f64>().unwrap();
+    benchmark_with_iter(
+        tensor_size,
+        num_tensors,
+        repeat,
+        core_num,
+        get_randn_tensors_candle,
+        candle_tensor_reduce_fn,
+    );
+
+    // benchmarking nalgebra
+    let nalgebra_matrix_reduce_fn = |t: &DMatrix<f32>| t.sum();
+    // use benchmark_with_into_iter will reduce speedup from 11x to 8x
+    benchmark_with_iter(
+        tensor_size,
+        num_tensors,
+        repeat,
+        core_num,
+        get_randn_tensors_nalgebra,
+        nalgebra_matrix_reduce_fn,
+    );
+
+    // benchmarking tch
+    let tch_tensor_reduce_fn = |t: Tensor| t.sum(Kind::Float).double_value(&[]);
+    set_num_interop_threads(core_num as i32);
+    set_num_threads(core_num as i32);
+    benchmark_with_into_iter(
+        tensor_size,
+        num_tensors,
+        repeat,
+        core_num,
+        get_randn_tensors_tch,
+        tch_tensor_reduce_fn,
+    );
+}
+
+fn benchmark_with_into_iter<T: Send, F: Send + std::iter::Sum>(
+    tensor_size: usize,
+    num_tensors: usize,
+    repeat: usize,
+    core_num: usize,
+    get_random_tensors_fn: impl Fn(usize, usize) -> Vec<T>,
+    tensor_reduce_fn: impl Fn(T) -> F + Sync + Send + Copy,
+) {
+    // warmup
+    let _results: F = get_random_tensors_fn(tensor_size, core_num)
+        .into_par_iter()
+        .map(tensor_reduce_fn)
+        .sum();
+    black_box(_results);
+    // use into_par_iter will reduce speedup from 11x to 8x
+    let avg_time_parallel = (0..repeat)
+        .map(|_| {
+            let tensors = get_random_tensors_fn(tensor_size, num_tensors);
+            let now = Instant::now();
+            let _sum: F = tensors.into_par_iter().map(tensor_reduce_fn).sum();
+            let duration = now.elapsed();
+            black_box(_sum);
+            duration.as_secs_f64()
+        })
+        .sum::<f64>()
+        / repeat as f64;
+
+    let avg_time_serial = (0..repeat)
+        .map(|_| {
+            let tensors = get_random_tensors_fn(tensor_size, num_tensors);
+            let now = Instant::now();
+            let _sum: F = tensors.into_iter().map(tensor_reduce_fn).sum();
+            let duration = now.elapsed();
+            black_box(_sum);
+            duration.as_secs_f64()
+        })
+        .sum::<f64>()
+        / repeat as f64;
+
+    println!(
+        "avg_time_parallel: {avg_time_parallel}s, avg_time_serial: {avg_time_serial}s, speedup: {}x",
+        avg_time_serial / avg_time_parallel
+    );
+}
+
+fn benchmark_with_iter<T: Send + Sync, F: Send + std::iter::Sum>(
+    tensor_size: usize,
+    num_tensors: usize,
+    repeat: usize,
+    core_num: usize,
+    get_random_tensors_fn: impl Fn(usize, usize) -> Vec<T>,
+    tensor_reduce_fn: impl Fn(&T) -> F + Sync + Send + Copy,
+) {
+    // warmup
+    let _results: F = get_random_tensors_fn(tensor_size, core_num)
+        .par_iter()
+        .map(tensor_reduce_fn)
+        .sum();
+    black_box(_results);
+    // use into_par_iter will reduce speedup from 11x to 8x
+    let avg_time_parallel = (0..repeat)
+        .map(|_| {
+            let tensors = get_random_tensors_fn(tensor_size, num_tensors);
+            let now = Instant::now();
+            let _sum: F = tensors.par_iter().map(tensor_reduce_fn).sum();
+            let duration = now.elapsed();
+            black_box(_sum);
+            duration.as_secs_f64()
+        })
+        .sum::<f64>()
+        / repeat as f64;
+
+    let avg_time_serial = (0..repeat)
+        .map(|_| {
+            let tensors = get_random_tensors_fn(tensor_size, num_tensors);
+            let now = Instant::now();
+            let _sum: F = tensors.iter().map(tensor_reduce_fn).sum();
+            let duration = now.elapsed();
+            black_box(_sum);
+            duration.as_secs_f64()
+        })
+        .sum::<f64>()
+        / repeat as f64;
+
+    println!(
+        "avg_time_parallel: {avg_time_parallel}s, avg_time_serial: {avg_time_serial}s, speedup: {}x",
+        avg_time_serial / avg_time_parallel
+    );
 }
 
 fn get_randn_tensors_burn(tensor_size: usize, num_tensors: usize) -> Vec<BurnTensor<NdArray, 2>> {
@@ -59,190 +191,6 @@ fn get_randn_tensors_candle(tensor_size: usize, num_tensors: usize) -> Vec<candl
             candle_core::Tensor::randn(0.0, 1.0, &[tensor_size, tensor_size], &device).unwrap()
         })
         .collect()
-}
-
-fn benchmark_parallelism_candle(tensor_size: usize, num_tensors: usize, repeat: usize) {
-    let core_num = num_cpus::get();
-    println!("core_num: {core_num}");
-    rayon::ThreadPoolBuilder::new()
-        .num_threads(core_num)
-        .build_global()
-        .unwrap();
-    // warmup
-    let results: f64 = get_randn_tensors_candle(tensor_size, core_num)
-        .par_iter()
-        .map(|t| t.sum([0, 1]).unwrap().to_scalar::<f64>().unwrap())
-        .sum();
-    assert!(results != 0.0);
-    // use into_par_iter will reduce speedup from 11x to 8x
-    let avg_time_parallel = (0..repeat)
-        .map(|_| {
-            let tensors = get_randn_tensors_candle(tensor_size, num_tensors);
-            let now = Instant::now();
-            let sum: f64 = tensors.par_iter().map(|t| t.sum([0, 1]).unwrap().to_scalar::<f64>().unwrap()).sum();
-            let duration = now.elapsed();
-            assert!(sum != 0.0);
-            duration.as_secs_f64()
-        })
-        .sum::<f64>()
-        / repeat as f64;
-
-    let avg_time_serial = (0..repeat)
-        .map(|_| {
-            let tensors = get_randn_tensors_candle(tensor_size, num_tensors);
-            let now = Instant::now();
-            let sum: f64 = tensors.iter().map(|t| t.sum([0, 1]).unwrap().to_scalar::<f64>().unwrap()).sum();
-            let duration = now.elapsed();
-            assert!(sum != 0.0);
-            duration.as_secs_f64()
-        })
-        .sum::<f64>()
-        / repeat as f64;
-
-    println!(
-        "avg_time_parallel: {avg_time_parallel}s, avg_time_serial: {avg_time_serial}s, speedup: {}x",
-        avg_time_serial / avg_time_parallel
-    );
-}
-
-fn benchmark_parallelism_burn(tensor_size: usize, num_tensors: usize, repeat: usize) {
-    let core_num = num_cpus::get();
-    println!("core_num: {core_num}");
-    rayon::ThreadPoolBuilder::new()
-        .num_threads(core_num)
-        .build_global()
-        .unwrap();
-    // warmup
-    let results: f32 = get_randn_tensors_burn(tensor_size, core_num)
-        .into_par_iter()
-        .map(|t| t.sum().into_scalar())
-        .sum();
-    assert!(results != 0.0);
-    // use into_par_iter will reduce speedup from 11x to 8x
-    let avg_time_parallel = (0..repeat)
-        .map(|_| {
-            let tensors = get_randn_tensors_burn(tensor_size, num_tensors);
-            let now = Instant::now();
-            let sum: f32 = tensors.into_par_iter().map(|t| t.sum().into_scalar()).sum();
-            let duration = now.elapsed();
-            assert!(sum != 0.0);
-            duration.as_secs_f64()
-        })
-        .sum::<f64>()
-        / repeat as f64;
-
-    let avg_time_serial = (0..repeat)
-        .map(|_| {
-            let tensors = get_randn_tensors_burn(tensor_size, num_tensors);
-            let now = Instant::now();
-            let sum: f32 = tensors.into_iter().map(|t| t.sum().into_scalar()).sum();
-            let duration = now.elapsed();
-            assert!(sum != 0.0);
-            duration.as_secs_f64()
-        })
-        .sum::<f64>()
-        / repeat as f64;
-
-    println!(
-        "avg_time_parallel: {avg_time_parallel}s, avg_time_serial: {avg_time_serial}s, speedup: {}x",
-        avg_time_serial / avg_time_parallel
-    );
-}
-
-fn benchmark_parallelism_nalgebra(tensor_size: usize, num_tensors: usize, repeat: usize) {
-    let core_num = num_cpus::get();
-    println!("core_num: {core_num}");
-    rayon::ThreadPoolBuilder::new()
-        .num_threads(core_num)
-        .build_global()
-        .unwrap();
-    // warmup
-    let results: f32 = get_randn_tensors_nalgebra(tensor_size, core_num)
-        .par_iter()
-        .map(|t| t.sum())
-        .sum();
-    assert!(results != 0.0);
-    // use into_par_iter will reduce speedup from 11x to 8x
-    let avg_time_parallel = (0..repeat)
-        .map(|_| {
-            let tensors = get_randn_tensors_nalgebra(tensor_size, num_tensors);
-            let now = Instant::now();
-            let sum: f32 = tensors.par_iter().map(|t| t.sum()).sum();
-            let duration = now.elapsed();
-            assert!(sum != 0.0);
-            duration.as_secs_f64()
-        })
-        .sum::<f64>()
-        / repeat as f64;
-
-    let avg_time_serial = (0..repeat)
-        .map(|_| {
-            let tensors = get_randn_tensors_nalgebra(tensor_size, num_tensors);
-            let now = Instant::now();
-            let sum: f32 = tensors.iter().map(|t| t.sum()).sum();
-            let duration = now.elapsed();
-            assert!(sum != 0.0);
-            duration.as_secs_f64()
-        })
-        .sum::<f64>()
-        / repeat as f64;
-
-    println!(
-        "avg_time_parallel: {avg_time_parallel}s, avg_time_serial: {avg_time_serial}s, speedup: {}x",
-        avg_time_serial / avg_time_parallel
-    );
-}
-
-fn benchmark_parallelism_tch(tensor_size: usize, num_tensors: usize, repeat: usize) {
-    let core_num = num_cpus::get();
-    set_num_threads(core_num as i32);
-    set_num_interop_threads(core_num as i32);
-    println!("core_num: {core_num}");
-    rayon::ThreadPoolBuilder::new()
-        .num_threads(core_num)
-        .build_global()
-        .unwrap();
-    // warmup
-    let results: f64 = get_randn_tensors_tch(tensor_size, core_num)
-        .into_par_iter()
-        .map(|t| t.sum(Kind::Float).double_value(&[]))
-        .sum();
-    assert!(results != 0.0);
-
-    let avg_time_parallel = (0..repeat)
-        .map(|_| {
-            let tensors = get_randn_tensors_tch(tensor_size, num_tensors);
-            let now = Instant::now();
-            let sum: f64 = tensors
-                .into_par_iter()
-                .map(|t| t.sum(Kind::Float).double_value(&[]))
-                .sum();
-            let duration = now.elapsed();
-            assert!(sum != 0.0);
-            duration.as_secs_f64()
-        })
-        .sum::<f64>()
-        / repeat as f64;
-
-    let avg_time_serial = (0..repeat)
-        .map(|_| {
-            let tensors = get_randn_tensors_tch(tensor_size, num_tensors);
-            let now = Instant::now();
-            let sum: f64 = tensors
-                .into_iter()
-                .map(|t| t.sum(Kind::Float).double_value(&[]))
-                .sum();
-            let duration = now.elapsed();
-            assert!(sum != 0.0);
-            duration.as_secs_f64()
-        })
-        .sum::<f64>()
-        / repeat as f64;
-
-    println!(
-        "avg_time_parallel: {avg_time_parallel}s, avg_time_serial: {avg_time_serial}s, speedup: {}x",
-        avg_time_serial / avg_time_parallel
-    );
 }
 
 #[cfg(test)]
